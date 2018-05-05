4d2b7b9f8e17cf6a3f0602d122c0450549d3b78b
ngx-devtools <aelbore.sg@gmail.com>
2018-05-05 10:55:20 +0800

    updates

---
+++ builder/index.js
@@ -0,0 +1,7 @@
+
+const { RouterFactory, ROUTE_METHOD } = require('./router');
+const Server = require('./server');
+
+exports.RouterFactory = RouterFactory;
+exports.ROUTE_METHOD = ROUTE_METHOD;
+exports.Server = Server;
