920cc512a19a619aa6c9c0baaa9a0e7b9d84ca63
aelbore <aelbore.sg@gmail.com>
2018-08-05 15:04:12 +0800

    convert to typescript

--- utils/app-route.js
+++
@@ -1,4 +0,0 @@
-module.exports = (app, routeDir) => {
-  const routeFiles = require('./glob').globSync(routeDir, [ '.js' ]).filter(file => file.includes('.route.js'));
-  if (routeFiles) routeFiles.forEach(file => app.use('/api', require(file)));
-};
