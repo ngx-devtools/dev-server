039a721bbafbd491e05ee1db14fab56134ee9d80
aelbore <aelbore.sg@gmail.com>
2018-10-06 12:46:06 +0800

    add hook

--- build.js
+++ build.js
@@ -1,15 +1,10 @@
-const { ngxBuild, writeFileAsync } =  require('@ngx-devtools/common');
+const { ngxBuild } =  require('@ngx-devtools/common');
 const { getRollupConfig } = require('./rollup.config');
+const { onAfterBuild } = require('./build-after');
 
 (async function(){
   const PKG_NAME = 'server';
-
-  const startContents = [
-    `const { Server } = require('./server');`,
-    `const server = new Server();`,
-    `server.listen();`
-  ]
-
+  
   await ngxBuild(PKG_NAME, getRollupConfig())
-  await writeFileAsync('dist/start.js', startContents.join('\n')) 
+  await onAfterBuild();
 })();
